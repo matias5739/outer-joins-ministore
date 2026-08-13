2ee9c32f45299e862f74c361ed628f15fb234c09
matias5739 <mmorelli95@outlook.com>
2026-08-13 02:13:59 -0300

    Add SQL queries for product and sales audits
    Added SQL queries to identify products with no sales and sales with uncategorized products.

--- soluciones.sql
+++ soluciones.sql
@@ -8,25 +8,44 @@
 -- Mostrá todos los productos y sus ventas asociadas.
 -- Los productos sin ventas aparecerán con NULL en las columnas de ventas.
 -- Los productos del catálogo que nunca fueron vendidos son el Hub USB-C 7p y el Parlante Bluetooth.
+SELECT 
+T1.nombre,
+T2.venta_id
+FROM productos AS T1
+LEFT JOIN ventas AS T2
+ON T1.producto_id=T2.producto_id
+WHERE venta_id IS NULL;
 -- ── CONSULTA 2: RIGHT JOIN ────────────────
 -- Pregunta de negocio: ¿Existen ventas registradas con productos
 -- que no figuran en nuestro catálogo? (posible error de carga de datos)
 -- Los registros huérfanos aparecerán con NULL en las columnas de productos.
 -- Si, existen producto no categorizados en nuestro catalagos que tuvieran ventas. venta_id 10, porducto_id 999
+SELECT 
+T1.nombre,
+T2.venta_id,
+T2.producto_id
+FROM productos AS T1
+RIGHT JOIN ventas AS T2
+ON T1.producto_id=T2.producto_id
+WHERE nombre IS NULL;
 -- ── CONSULTA 3: FULL OUTER JOIN ───────────
 -- Pregunta de negocio: Vista completa de auditoría que muestre
 -- todos los productos y todas las ventas sin perder ninguna fila,
 -- identificando tanto productos sin ventas como ventas sin producto.
-nombre                venta_id    producto_id
-Laptop Pro 15      	      1          	101
-Laptop Pro 15	           4      	    101
-Mouse Inalámbrico	        2      	    102
-Mouse Inalámbrico    	    8        	  102
-Monitor 4K 27"      	    3        	  103
-Teclado Mecánico	        5	          104
-Auriculares BT Pro	      6        	  105
-SSD Externo 1TB	          7      	    106
-Webcam HD 1080p	          9	          107
-Hub USB-C 7p	            NULL	      NULL
-Parlante Bluetooth	      NULL    	  NULL
-NULL	                    10	        999
+
+SELECT 
+T1.nombre,
+T2.venta_id
+FROM productos AS T1
+LEFT JOIN ventas AS T2
+ON T1.producto_id=T2.producto_id
+WHERE venta_id IS NULL
+UNION
+SELECT 
+T1.nombre,
+T2.venta_id,
+T2.producto_id
+FROM productos AS T1
+RIGHT JOIN ventas AS T2
+ON T1.producto_id=T2.producto_id
+WHERE nombre IS NULL;
